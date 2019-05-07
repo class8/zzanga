834a40a818bfb9b77e0e7417978af70cdfcf8229
HwangJeson <kkoohh319@naver.com>
2019-05-07 11:22:08 +0900

    test

---
+++ Test/src/VaTest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class VaTest {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+
+		int num = 0;
+
+		System.out.println("숫자입력: ");
+		num = input.nextInt();
+
+		/*
+		 * try { System.out.println("숫자입력: "); num = input.nextInt(); } catch
+		 * (ArithmeticException e) { System.out.println(e.toString());
+		 * System.out.println("입력 오류입니다."); } catch (Exception e) {
+		 * System.out.println(e.toString()); System.out.println("입력 오류입니다."); } finally
+		 * { System.out.println(num); }
+		 */
+		
+		// 교재 612- 630 - 예제 풀어보기 
+
+	}
+
+}
