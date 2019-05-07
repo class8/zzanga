81dc9f5da322225cc90d0ae1cb3ede601566be52
JinWoo Choi <kaishail0307@gmail.com>
2019-05-07 21:43:49 +0900

    main 수정

--- UniversityRegisterCourses/src/control/DBUtil.java
+++ UniversityRegisterCourses/src/control/DBUtil.java
@@ -9,7 +9,7 @@ public class DBUtil { // 데이터베이스 연동
 	static final String url = "jdbc:oracle:thin:@127.0.0.1:1521:orcl";
 
 	public static Connection getConnection() throws Exception {
-		
+
 		Class.forName(driver);
 		Connection con = DriverManager.getConnection(url, "scott", "tiger");
 		System.out.println("DB 연결에 성공하였습니다.");
