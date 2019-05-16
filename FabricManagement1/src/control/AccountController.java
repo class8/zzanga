a9d4fed232b95d5c8fcc1278d6453732b3b5f084
JinWoo Choi <kaishail0307@gmail.com>
2019-05-16 17:46:34 +0900

    수정5:46

--- FabricManagement1/src/control/AccountController.java
+++ FabricManagement1/src/control/AccountController.java
@@ -1,14 +1,25 @@
 package control;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import model.AccountVO;
 
 public class AccountController implements Initializable {
 
@@ -22,104 +33,230 @@ public class AccountController implements Initializable {
 	TextField a_txtPhone;
 	@FXML
 	TextField a_txtEmail;
+	@FXML
+	TextField a_txtAddress;
+	@FXML
+	TextField a_txtBnumber;
+	@FXML
+	TextField a_txtMsubject;
+	@FXML
+	TextArea a_txtRemarks;
+	@FXML
+	Button a_btnRegist;
+	@FXML
+	Button a_btnInit;
+	@FXML
+	Button a_btnUpdate;
+	@FXML
+	Button a_btnDelete;
+	@FXML
+	Button a_btnExit;
+	@FXML
+	TextField a_txtSearch;
+	@FXML
+	Button a_btnSearch;
+	@FXML
+	private TableView<AccountVO> a_tableView = new TableView<>();
 
-	
-	
-
-
+	ObservableList<AccountVO> accountDataList = FXCollections.observableArrayList();
+	ObservableList<AccountVO> selectAccount = null; // 학생등록 테이블에서 선택한 정보 저장
+	int selectedAccountIndex; // 학생 등록 탭에서 선택한 학과 정보 인덱스 저장
+	String accountNumber = "";
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		try {
-			// 학생 등록 초기화
 			a_btnUpdate.setDisable(true);
-			btnStudentUpdate.setDisable(true);
-			studentTableView.setEditable(false);
+			a_btnDelete.setDisable(true);
 
-			// 학번 수정금지
-			txtsd_num.setEditable(false);
+			TableColumn colAnumber = new TableColumn("번호");
+			colAnumber.setPrefWidth(40);
+			colAnumber.setStyle("-fx-alignment: CENTER");
+			colAnumber.setCellValueFactory(new PropertyValueFactory<>("a_number"));
 
-			// 학생 테이블 뷰 컬럼이름 설정
-			@SuppressWarnings("rawtypes")
+			TableColumn colAcname = new TableColumn("거래처명");
+			colAcname.setPrefWidth(90);
+			colAcname.setStyle("-fx-alignment: CENTER");
+			colAcname.setCellValueFactory(new PropertyValueFactory<>("a_cname"));
 
-			TableColumn colStudentNo = new TableColumn("NO.");
-			colStudentNo.setPrefWidth(30);
-			colStudentNo.setStyle("-fx-alignment: CENTER");
-			colStudentNo.setCellValueFactory(new PropertyValueFactory<>("no"));
+			TableColumn colAmname = new TableColumn("담당자명");
+			colAmname.setPrefWidth(90);
+			colAmname.setStyle("-fx-alignment: CENTER");
+			colAmname.setCellValueFactory(new PropertyValueFactory<>("a_mname"));
 
-			TableColumn colStudentNum = new TableColumn("학번");
-			colStudentNum.setPrefWidth(70);
-			colStudentNum.setStyle("-fx-alignment: CENTER");
-			colStudentNum.setCellValueFactory(new PropertyValueFactory<>("sd_num"));
+			TableColumn colAphone = new TableColumn("연락처");
+			colAphone.setPrefWidth(100);
+			colAphone.setStyle("-fx-alignment: CENTER");
+			colAphone.setCellValueFactory(new PropertyValueFactory<>("a_phone"));
 
-			TableColumn colStudentName = new TableColumn("이름");
-			colStudentName.setPrefWidth(80);
-			colStudentName.setStyle("-fx-alignment: CENTER");
-			colStudentName.setCellValueFactory(new PropertyValueFactory<>("sd_name"));
+			TableColumn colAemail = new TableColumn("이메일");
+			colAemail.setPrefWidth(100);
+			colAemail.setStyle("-fx-alignment: CENTER");
+			colAemail.setCellValueFactory(new PropertyValueFactory<>("a_email"));
 
-			TableColumn colStudentId = new TableColumn("아이디");
-			colStudentId.setPrefWidth(80);
-			colStudentId.setStyle("-fx-alignment: CENTER");
-			colStudentId.setCellValueFactory(new PropertyValueFactory<>("sd_id"));
+			TableColumn colAbnumber = new TableColumn("사업자번호");
+			colAbnumber.setPrefWidth(100);
+			colAbnumber.setStyle("-fx-alignment: CENTER");
+			colAbnumber.setCellValueFactory(new PropertyValueFactory<>("a_bnumber"));
 
-			TableColumn colStudentPassword = new TableColumn("비밀번호");
-			colStudentPassword.setPrefWidth(80);
-			colStudentPassword.setStyle("-fx-alignment: CENTER");
-			colStudentPassword.setCellValueFactory(new PropertyValueFactory<>("sd_passwd"));
+			TableColumn colAmsubject = new TableColumn("주종목");
+			colAmsubject.setPrefWidth(100);
+			colAmsubject.setStyle("-fx-alignment: CENTER");
+			colAmsubject.setCellValueFactory(new PropertyValueFactory<>("a_msubject"));
 
-			TableColumn colSubjectNum = new TableColumn("학과명");
-			colSubjectNum.setPrefWidth(70);
-			colSubjectNum.setStyle("-fx-alignment: CENTER");
-			colSubjectNum.setCellValueFactory(new PropertyValueFactory<>("s_num"));
+			TableColumn colAaddress = new TableColumn("주소");
+			colAaddress.setPrefWidth(150);
+			colAaddress.setStyle("-fx-alignment: CENTER");
+			colAaddress.setCellValueFactory(new PropertyValueFactory<>("a_address"));
 
-			TableColumn colStudentBirthday = new TableColumn("생년월일");
-			colStudentBirthday.setPrefWidth(80);
-			colStudentBirthday.setStyle("-fx-alignment: CENTER");
-			colStudentBirthday.setCellValueFactory(new PropertyValueFactory<>("sd_birthday"));
+			TableColumn colAremarks = new TableColumn("비고");
+			colAremarks.setPrefWidth(150);
+			colAremarks.setStyle("-fx-alignment: CENTER");
+			colAremarks.setCellValueFactory(new PropertyValueFactory<>("a_remarks"));
 
-			TableColumn colStudentPhone = new TableColumn("연락처");
-			colStudentPhone.setPrefWidth(80);
-			colStudentPhone.setStyle("-fx-alignment: CENTER");
-			colStudentPhone.setCellValueFactory(new PropertyValueFactory<>("sd_phone"));
+			TableColumn colAregistdate = new TableColumn("등록일");
+			colAregistdate.setPrefWidth(90);
+			colAregistdate.setStyle("-fx-alignment: CENTER");
+			colAregistdate.setCellValueFactory(new PropertyValueFactory<>("a_registdate"));
 
-			TableColumn colStudentAddress = new TableColumn("주 소");
-			colStudentAddress.setPrefWidth(150);
-			colStudentAddress.setStyle("-fx-alignment: CENTER");
-			colStudentAddress.setCellValueFactory(new PropertyValueFactory<>("sd_address"));
+			a_tableView.setItems(accountDataList);
 
-			TableColumn colStudentEmail = new TableColumn("이메일");
-			colStudentEmail.setPrefWidth(80);
-			colStudentEmail.setStyle("-fx-alignment: CENTER");
-			colStudentEmail.setCellValueFactory(new PropertyValueFactory<>("sd_email"));
+			a_tableView.getColumns().addAll(colAnumber, colAcname, colAmname, colAphone, colAemail, colAbnumber,
+					colAmsubject, colAaddress, colAremarks, colAregistdate);
 
-			TableColumn colStudentDate = new TableColumn("등록일");
-			colStudentDate.setPrefWidth(80);
-			colStudentDate.setStyle("-fx-alignment: CENTER");
-			colStudentDate.setCellValueFactory(new PropertyValueFactory<>("sd_date"));
+			// 전체 목록
+			accountTotalList();
 
-			studentTableView.setItems(studentDataList);
-
-			studentTableView.getColumns().addAll(colStudentNo, colStudentNum, colStudentName, colStudentId,
-					colStudentPassword, colSubjectNum, colStudentBirthday, colStudentPhone, colStudentAddress,
-					colStudentEmail, colStudentDate);
-
-			// 학생 전체 목록
-			studentTotalList();
-
-			// 추가된 학과명 호출
-			// addSubjectName();
-
-			btnStudentInsert.setOnAction(event -> handlerBtnStudentInsertAction(event));
-			cbx_subjectName.setOnAction(event -> handlerCbx_subjectNameAction(event));
-			btnIdCheck.setOnAction(event -> handlerBtnIdCheckAction(event));
-			studentTableView.setOnMouseClicked(event -> handlerStudentTableViewAction(event));
-			btnStudentUpdate.setOnAction(event -> handlerBtnStudentUpdateAction(event));
-			btnStudentInit.setOnAction(event -> handlerBtnStudentInitAction(event));
-			btnStudentTotalList.setOnAction(event -> handlerBtnStudentTotalListAction(event));
+			a_btnRegist.setOnAction(event -> handlerBtnRegistAction(event));
+			a_btnInit.setOnAction(event -> handlerBtnInitAction(event));
+			a_btnUpdate.setOnAction(event -> handlerBtnUpdateAction(event));
+			a_btnDelete.setOnAction(event -> handlerBtnDeleteAction(event));
+			a_btnExit.setOnAction(event -> handlerBtnExitAction(event));
+			a_btnSearch.setOnAction(event -> handlerBtnSearchAction(event));
+			a_tableView.setOnMouseClicked(event -> handlerAccountTableViewAction(event));
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	private Object handlerAccountTableViewAction(MouseEvent event) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	// 등록버튼 이벤트
+	public void handlerBtnRegistAction(ActionEvent event) {
+		try {
+			accountDataList.removeAll(accountDataList);
+
+			AccountVO avo = null;
+			AccountDAO adao = null;
+
+			avo = new AccountVO(a_txtCname.getText().trim(), a_txtMname.getText().trim(), a_txtPhone.getText().trim(),
+					a_txtEmail.getText().trim(), a_txtAddress.getText().trim(), a_txtBnumber.getText().trim(),
+					a_txtMsubject.getText().trim(), a_txtRemarks.getText().trim());
+			adao = new AccountDAO();
+			adao.getAccountRegist(avo);
+
+			if (adao != null) {
+				accountTotalList();
+
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("학생 입력");
+				alert.setHeaderText(a_txtCname.getText() + " 거래처가 성공적으로 추가되었습니다.");
+				alert.setContentText("다음 거래처를 입력하세요.");
+				alert.showAndWait();
+
+				a_txtNumber.clear();
+				a_txtCname.clear();
+				a_txtMname.clear();
+				a_txtPhone.clear();
+				a_txtEmail.clear();
+				a_txtAddress.clear();
+				a_txtBnumber.clear();
+				a_txtMsubject.clear();
+				a_txtRemarks.clear();
+				a_txtCname.requestFocus();
+			}
+		} catch (Exception e) {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("학과 정보 입력");
+			alert.setHeaderText("학과 정보를 정확히 입력하세요.");
+			alert.setContentText("다음에는 주의하세요.");
+			alert.showAndWait();
+		}
+	}
+
+	// 전체 목록
+	public void accountTotalList() throws Exception {
+		Object[][] totalData;
+
+		AccountDAO aDao = new AccountDAO();
+		AccountVO aVo = null;
+		ArrayList<String> title;
+		ArrayList<AccountVO> list;
+
+		title = aDao.getAccountColumnName();
+		int columnCount = title.size();
+
+		list = aDao.getAccountTotalList();
+		int rowCount = list.size();
+
+		totalData = new Object[rowCount][columnCount];
+
+		for (int index = 0; index < rowCount; index++) {
+			aVo = list.get(index);
+			accountDataList.add(aVo);
+		}
+	}
+
+	// 전체 목록
+	public void handlerBtnStudentTotalListAction(ActionEvent event) {
+		try {
+			accountDataList.removeAll(accountDataList);
+			accountTotalList();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void handlerBtnInitAction(ActionEvent event) {
+		try {
+			accountDataList.removeAll(accountDataList);
+			accountTotalList();
+
+			a_txtNumber.clear();
+			a_txtCname.clear();
+			a_txtMname.clear();
+			a_txtPhone.clear();
+			a_txtEmail.clear();
+			a_txtAddress.clear();
+			a_txtBnumber.clear();
+			a_txtMsubject.clear();
+			a_txtRemarks.clear();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void handlerBtnUpdateAction(ActionEvent event) {
+
+	}
+
+	private Object handlerBtnDeleteAction(ActionEvent event) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void handlerBtnExitAction(ActionEvent event) {
+		Platform.exit();
+	}
+
+	private Object handlerBtnSearchAction(ActionEvent event) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- FabricManagement1/src/control/AccountDAO.java
+++ FabricManagement1/src/control/AccountDAO.java
@@ -1,6 +1,201 @@
 package control;
 
-public class AccountDAO {
-	
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import model.AccountVO;
+
+public class AccountDAO {
+
+	// 거래처 등록
+	public void getAccountRegist(AccountVO avo) throws Exception {
+
+		String sql = "insert into account values " + "(account_seq.nextval, ?, ?, ?, ?, ?, ?, ?, ?, sysdate)";
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, avo.getA_cname());
+			pstmt.setString(2, avo.getA_mname());
+			pstmt.setString(3, avo.getA_phone());
+			pstmt.setString(4, avo.getA_email());
+			pstmt.setString(5, avo.getA_address());
+			pstmt.setString(6, avo.getA_bnumber());
+			pstmt.setString(7, avo.getA_msubject());
+			pstmt.setString(8, avo.getA_remarks());
+
+			int i = pstmt.executeUpdate();
+
+			if (i == 1) {
+
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("거래처 등록");
+				alert.setHeaderText(avo.getA_cname() + " 거래처 등록 완료.");
+				alert.setContentText("거래처 등록 성공!");
+				alert.showAndWait();
+
+			} else {
+
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setTitle("거래처 등록");
+				alert.setHeaderText("거래처 등록 실패");
+				alert.setContentText("거래처 등록 실패!");
+				alert.showAndWait();
+			}
+
+		} catch (SQLException e) {
+
+			System.out.println("e=[" + e + "]");
+
+		} catch (Exception e) {
+
+			System.out.println("e=[" + e + "]");
+
+		} finally {
+
+			try {
+
+				// 데이터베이스와의 연결에 사용되었던 오브젝트를 해제한다.
+				if (pstmt != null)
+					pstmt.close();
+
+				if (con != null)
+					con.close();
+
+			} catch (SQLException e) {
+
+			}
+		}
+
+	}
+
+	// 데이터베이스에서 거래처 테이블의 컬럼 갯수
+	public ArrayList<String> getAccountColumnName() throws Exception {
+
+		ArrayList<String> columnName = new ArrayList<String>();
+
+		String sql = "select * from account order by a_number";
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		// ResultSetMetaData 객체 변수 선언
+		ResultSetMetaData rsmd = null;
+
+		try {
+
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			rsmd = rs.getMetaData();
+
+			int cols = rsmd.getColumnCount();
+
+			for (int i = 1; i <= cols; i++) {
+
+				columnName.add(rsmd.getColumnName(i));
+			}
+
+		} catch (SQLException se) {
+
+			System.out.println(se);
+
+		} catch (Exception e) {
+
+			System.out.println(e);
+
+		} finally {
+
+			try {
+
+				// 데이터베이스와의 연결에 사용되었던 오브젝트를 해제한다.
+				if (rs != null)
+					rs.close();
+
+				if (pstmt != null)
+					pstmt.close();
+
+				if (con != null)
+					con.close();
+
+			} catch (SQLException se) {
+
+			}
+		}
+
+		return columnName;
+	}
+
+	// 학생 전체 목록
+	public ArrayList<AccountVO> getAccountTotalList() throws Exception {
+
+		ArrayList<AccountVO> list = new ArrayList<>();
+
+		String sql = "select a_number, a_cname, a_mname, a_phone, a_email, a_bnumber, a_msubject, a_address, a_remarks, a_registdate "
+				+ " from account" + " order by a_number";
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		AccountVO aVo = null;
+
+		try {
+
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+
+				aVo = new AccountVO();
+				aVo.setA_number(rs.getInt("a_number"));
+				aVo.setA_cname(rs.getString("a_cname"));
+				aVo.setA_mname(rs.getString("a_mname"));
+				aVo.setA_phone(rs.getString("a_phone"));
+				aVo.setA_email(rs.getString("a_email"));
+				aVo.setA_bnumber(rs.getString("a_bnumber"));
+				aVo.setA_msubject(rs.getString("a_msubject"));
+				aVo.setA_address(rs.getString("a_address"));
+				aVo.setA_remarks(rs.getString("a_remarks"));
+				aVo.setA_registdate(rs.getDate("a_registdate") + "");
+
+				list.add(aVo);
+			}
+
+		} catch (SQLException se) {
+
+			System.out.println(se);
+
+		} catch (Exception e) {
+
+			System.out.println(e);
+
+		} finally {
+
+			try {
+
+				// 데이터베이스와의 연결에 사용되었던 오브젝트를 해제한다.
+				if (rs != null)
+					rs.close();
+
+				if (pstmt != null)
+					pstmt.close();
+
+				if (con != null)
+					con.close();
+
+			} catch (SQLException se) {
+
+			}
+		}
+
+		return list;
+	}
 }
