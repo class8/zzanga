597fd4f982901db203aadc025527ddb1c22ed6ab
JinWoo Choi <kaishail0307@gmail.com>
2019-05-16 09:05:07 +0900

    어제작업분

--- FabricManagement1/src/control/LoginController.java
+++ FabricManagement1/src/control/LoginController.java
@@ -1,16 +1,138 @@
 package control;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import control.LoginDAO;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class LoginController implements Initializable {
 
+	@FXML
+	private TextField l_txtId;
+	@FXML
+	private PasswordField l_txtPassword;
+	@FXML
+	private Button l_btnLogin;
+	@FXML
+	private Button l_btnExit;
+
+	public static String idName;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		l_txtId.setOnKeyPressed(event -> handerTxtIdKeyPressed(event)); // 아이디 입력에서 Enter키 이벤트 적용
+		l_txtPassword.setOnKeyPressed(event -> handerTxtPasswordKeyPressed(event)); // 패스워드 입력에서 Enter키 이벤트 적용
+		l_btnLogin.setOnAction(event -> handlerBtnLoginAction(event));
+		l_btnExit.setOnAction(event -> handlerBtnExitAction(event));
 
 	}
 
+	// 아이디 입력에서 Enter키 이벤트 적용
+	public void handerTxtIdKeyPressed(KeyEvent event) {
+		if (event.getCode() == KeyCode.ENTER) {
+			l_txtPassword.requestFocus();
+		}
+	}
+
+	// 패스워드 입력에서 Enter키 이벤트 적용
+	public void handerTxtPasswordKeyPressed(KeyEvent event) {
+		if (event.getCode() == KeyCode.ENTER) {
+			login();
+		}
+	}
+
+	// 로그인창 닫기
+	public void handlerBtnExitAction(ActionEvent event) {
+		Platform.exit();
+	}
+
+	// 로그인
+	public void handlerBtnLoginAction(ActionEvent event) {
+		login();
+	}
+
+	// 로그인 메소드
+	public void login() {
+		LoginDAO login = new LoginDAO();
+
+		boolean sucess = false;
+
+		try {
+			idName = loginName();
+			sucess = login.getLogin(l_txtId.getText().trim(), l_txtPassword.getText().trim());
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+
+		Alert alert;
+		if (l_txtId.getText().equals("") || l_txtPassword.getText().equals("")) {
+			alert = new Alert(AlertType.WARNING);
+			alert.setTitle("로그인 실패");
+			alert.setHeaderText("아이디와 비밀번호 미입력");
+			alert.setContentText("아이디와 비밀번호중 입력하지 않은 항목이 있습니다." + "\n 다시 제대로 입력하시오.");
+			alert.setResizable(false);
+			alert.showAndWait();
+		}
+
+		// 로그인 성공시 메인 페이지로 이동
+		if (sucess) {
+			try {
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/mainView.fxml"));
+				Parent mainView = (Parent) loader.load();
+				Scene scane = new Scene(mainView);
+				Stage mainMtage = new Stage();
+				mainMtage.setTitle("원단 관리 프로그램");
+				mainMtage.setResizable(false);
+				mainMtage.setScene(scane);
+				Stage oldStage = (Stage) l_btnLogin.getScene().getWindow();
+				oldStage.close();
+				mainMtage.show();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.err.println("오류" + e);
+			}
+		} else {
+			// 아이디패스워드 확인하라는 창
+			alert = new Alert(AlertType.WARNING);
+			alert.setTitle("로그인 실패");
+			alert.setHeaderText("아이디와 비밀번호 불일치");
+			alert.setContentText("아이디와 비밀번호가 일치하지 않습니다." + "\n 다시 제대로 입력하시오.");
+			alert.setResizable(false);
+			alert.showAndWait();
+
+			l_txtId.clear();
+			l_txtPassword.clear();
+		}
+	}
+
+	// 아이디 불러오기
+	public String loginName() {
+		LoginDAO ldao = new LoginDAO();
+
+		String id = null;
+
+		try {
+			id = ldao.getLoginId(l_txtId.getText());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return id;
+	}
 }
--- FabricManagement1/src/control/LoginDAO.java
+++ FabricManagement1/src/control/LoginDAO.java
@@ -1,5 +1,84 @@
 package control;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import control.DBUtil;
+
 public class LoginDAO {
+	// 아이디 중복 체크
+	public boolean getLogin(String loginId, String loginPassword) throws Exception {
+
+		String sql = "select * from admin where id = ? and password = ?";
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		boolean loginResult = false;
+
+		try {
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, loginId);
+			pstmt.setString(2, loginPassword);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				loginResult = true; // 아이디와 패스워드가 일치한다.
+			}
+
+		} catch (SQLException e) {
+			System.out.println("e=[" + e + "]");
+		} catch (Exception e) {
+			System.out.println("e=[" + e + "]");
+		} finally {
+			try {
+				// 데이터베이스와의 연결에 사용되었던 오브젝트를 해제
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+			}
+		}
+		return loginResult;
+	}
+
+	public String getLoginId(String loginId) throws Exception {
+
+		String sql = "select name from admin where id = ?";
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String loginId1 = null;
+
+		try {
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, loginId);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				loginId1 = rs.getString(1);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("e=[" + e + "]");
+		} catch (Exception e) {
+			System.out.println("e=[" + e + "]");
+		} finally {
+			try {
+				// 데이터베이스와의 연결에 사용되었던 오브젝트를 해제
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+			}
+		}
+		return loginId1;
+	}
 
 }
