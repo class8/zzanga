98de34f57b18f1ec0ab571aa2fe39e5750211986
JinWoo Choi <kaishail0307@gmail.com>
2019-05-19 19:54:22 +0900

    테스트

---
+++ fabricmanagement.sql
@@ -0,0 +1,62 @@
+select *
+from tab;
+
+
+create table administrator(
+id varchar2(20),
+name varchar2(20) not null,
+password varchar2(20) not null,
+primary key(id)
+);
+
+drop table administrator;
+
+insert into administrator(id,name,password)
+VALUES ('admin','admin','admin');
+
+select *
+from administrator;
+
+drop table account;
+
+create table account(
+a_number number,
+a_cname varchar2(15) not null,
+a_mname varchar2(15) not null,
+a_phone varchar2(15) not null,
+a_email varchar2(50) not null,
+a_address varchar2(100) not null,
+a_bnumber varchar2(20) unique not null,
+a_msubject varchar2(20) not null,
+a_remarks varchar2(50),
+a_registdate date DEFAULT sysdate not null,
+primary key(a_number)   
+);
+
+drop sequence account_seq;
+
+create sequence account_seq
+start with 1
+increment by 1;
+
+select account_seq.nextval from dual;
+
+create table fabric(
+f_number varchar2,
+a_cname varchar2(15) not null,
+a_mname varchar2(15) not null,
+a_phone varchar2(15) not null,
+a_email varchar2(50) not null,
+a_address varchar2(100) not null,
+a_bnumber varchar2(20) unique not null,
+a_msubject varchar2(20) not null,
+a_remarks varchar2(50),
+a_registdate date DEFAULT sysdate not null,
+primary key(a_number)   
+);
+
+desc account;
+
+select *
+from account;
+
