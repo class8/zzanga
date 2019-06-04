0bb6cb3fad900c57675c61ec62fef607419136b6
HwangJeson <kkoohh319@naver.com>
2019-06-04 17:43:54 +0900

    spd ~

--- FabricManagement1/src/control/DBUtil.java
+++ FabricManagement1/src/control/DBUtil.java
@@ -1,12 +1,11 @@
 package control;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBUtil { // 데이터베이스 연동
 
 	static final String driver = "oracle.jdbc.driver.OracleDriver";
-	static final String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+	static final String url = "jdbc:oracle:thin:@192.168.0.104:1521:orcl";
 
 	public static Connection getConnection() throws Exception {
 
